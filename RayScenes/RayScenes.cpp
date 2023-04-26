188fa423ed8a00122413b2d0f5fc5120979bf476
Benjamin <benjamin.gerbe@gmail.com>
2023-04-26 18:34:54 +0200

    initial commit

---
+++ RayScenes/RayScenes.cpp
@@ -0,0 +1,12 @@
+﻿// RayScenes.cpp : Defines the entry point for the application.
+//
+
+#include "RayScenes.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
